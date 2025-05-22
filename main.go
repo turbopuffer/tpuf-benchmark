0860aa57fcd0915c8f4bd6354b9aef1df2076a5e
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2025-05-22 01:23:16 -0400

    deep1b

--- main.go
+++ main.go
@@ -119,7 +119,7 @@ func run(ctx context.Context, shutdown context.CancelFunc) error {
 	log.Print("sanity check passed")
 
 	// Setup namespaces
-	executor.vectors = NewCohereVectorSource()
+	executor.vectors = NewDeepVectorSource()
 	namespaces, sizes, err := setupNamespaces(
 		ctx,
 		httpClient,
