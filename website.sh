f80b7220960930f4a581d95f40c73a5fac2941d0
Simon Eskildsen <sirup@sirupsen.com>
2025-08-05 09:35:34 -0400

    more cold time

--- website.sh
+++ website.sh
@@ -75,7 +75,7 @@ vector-cold)
         "templates/query_cold.json.tmpl" \
         "templates/upsert_default.json.tmpl" \
         1000000 \
-        15m \
+        30m \
         "--purge-cache" \
         3
     ;;
@@ -101,7 +101,7 @@ fulltext-cold)
         "templates/query_full_text_cold.json.tmpl" \
         "templates/upsert_full_text.json.tmpl" \
         1000000 \
-        15m \
+        30m \
         "--purge-cache" \
         3
     ;;
